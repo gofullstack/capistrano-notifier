0a9f1eebefc1821e34306c4439583dc534820adb
Justin Campbell <justin@justincampbell.me>
2012-05-02 15:16:13 -0400

    Refactor Base#branch spec

--- spec/capistrano/notifier/base_spec.rb
+++ spec/capistrano/notifier/base_spec.rb
@@ -6,12 +6,17 @@ describe Capistrano::Notifier::Base do
   let(:configuration) { Capistrano::Configuration.new }
   subject { described_class.new configuration }
 
-  it 'uses configured branch if not specified in configuration' do
-    configuration.load { set :branch, 'foo' }
-    subject.send(:branch).should == 'foo'
+  it "defaults to the master branch" do
+    subject.send(:branch).should == 'master'
   end
 
-  it 'uses master branch if not specified in configuration' do
-    subject.send(:branch).should == 'master'
+  context "when a branch is specified" do
+    before :each do
+      configuration.load { set :branch, 'foo' }
+    end
+
+    it "uses the specified branch" do
+      subject.send(:branch).should == 'foo'
+    end
   end
 end
