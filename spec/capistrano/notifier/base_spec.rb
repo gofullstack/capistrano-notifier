7035f8a9118e1481f69eb1fd1f517c4e6072e662
Justin Campbell <justin@justincampbell.me>
2012-05-02 15:21:09 -0400

    Describe #branch

--- spec/capistrano/notifier/base_spec.rb
+++ spec/capistrano/notifier/base_spec.rb
@@ -6,17 +6,19 @@ describe Capistrano::Notifier::Base do
   let(:configuration) { Capistrano::Configuration.new }
   subject { described_class.new configuration }
 
-  it "defaults to the master branch" do
-    subject.send(:branch).should == 'master'
-  end
-
-  context "when a branch is specified" do
-    before :each do
-      configuration.load { set :branch, 'foo' }
+  describe "#branch" do
+    it "defaults to master" do
+      subject.send(:branch).should == 'master'
     end
 
-    it "uses the specified branch" do
-      subject.send(:branch).should == 'foo'
+    context "when a branch is specified" do
+      before :each do
+        configuration.load { set :branch, 'foo' }
+      end
+
+      it "uses the specified branch" do
+        subject.send(:branch).should == 'foo'
+      end
     end
   end
 end
