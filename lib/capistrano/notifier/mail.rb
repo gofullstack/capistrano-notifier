bf0bb4f5788d0b4233523dc42e407715a9481b68
Brian <bcobb@uwalumni.com>
2012-05-22 16:50:24 -0700

    Fix regression with current ActionMailer

--- lib/capistrano/notifier/mail.rb
+++ lib/capistrano/notifier/mail.rb
@@ -8,11 +8,11 @@ end
 
 class Capistrano::Notifier::Mailer < ActionMailer::Base
 
-  if respond_to?(:mail)
+  if ActionMailer::Base.respond_to?(:mail)
     def notice(text, from, subject, to, delivery_method)
       mail({
         :body => text,
-        :delivery_method => notify_method,
+        :delivery_method => delivery_method,
         :from => from,
         :subject => subject,
         :to => to
@@ -65,8 +65,7 @@ class Capistrano::Notifier::Mail < Capistrano::Notifier::Base
   end
 
   def perform_with_action_mailer(notifier = Capistrano::Notifier::Mailer)
-    notifier.notice(text, from, subject, to, notify_method)
-    notifier.deliver
+    notifier.notice(text, from, subject, to, notify_method).deliver
   end
 
   def body
